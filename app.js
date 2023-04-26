b660ada5bc0c9c5659483c5dc7ee3638272e86ce
Lucas G Ramos <47905641+LGR256@users.noreply.github.com>
2023-04-26 11:29:23 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -44,6 +44,32 @@ app.get("/excluir/:id", function(req, res){
     })
 })
 
+app.get("/editar/:id", function(req, res){
+    post.findAll({ 
+        where: { id: req.params.id}
+    }).then(function(post){
+            res.render("editar", {post})
+
+        
+    }).catch(function(erro){
+        res.send("Falha ao editar os dados: " + erro)
+    })
+})
+
+app.post("/atualizar", function(req, res){
+    post.update({
+        nome: req.body.nome,
+        telefone: req.body.telefone,
+        origem: req.body.origem,
+        data_contato: req.body.data_contato,
+        observacao: req.body.observacao
+    },{ where: {id: req.body.id}
+    }).then(function(){
+        res.redirect("/consulta")
+    }).catch(function(erro){
+        res.send("Falha ao cadastrar os dados: " + erro)
+    })
+})
 
 app.listen(8081, function(){
     console.log("Servidor ativo!")
